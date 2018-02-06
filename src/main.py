79277c3dceb59bfa7338e8269df3f5d20708e291
Maxime St-Pierre <me@maximest-pierre.me>
2018-02-06 12:50:40 -0500

    Add cors

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import json
 import requests
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods = ['GET'])
 def resolve_name():
