1c20d9a4c4c413015de6630b048c601201b710d1
ポール ウェッブ <netopwibby@thenetwork.email>
2018-07-18 11:00:50 -0400

    Minor fixes

--- src/main.py
+++ src/main.py
@@ -8,36 +8,40 @@ CORS(app)
 app.config.from_json("config.json")
 
 
-@app.route('/', methods = ['GET'])
+
+@app.route("/", methods = ["GET"])
 def resolve_name():
     param = request.args.to_dict()
+
     try:
-        method = param.pop('method')
-        access_token = param.pop('access_token')
+        method = param.pop("method")
+        access_token = param.pop("access_token")
     except:
-        return "<H1> Wrong parameters </H1>"
-    
-    if access_token != app.config['ACCESS_TOKEN']:
-        return "<H1> Invalid access token </H1>"
-    pretty_print = param.pop('pretty_print', False)
+        return "<h1>Wrong parameters</h1>"
+
+    if access_token !== app.config["ACCESS_TOKEN"]:
+        return "<h1>Invalid access token</h1>"
+
+    pretty_print = param.pop("pretty_print", False)
 
     response = api(method, param)
     print (pretty_print)
-    if(pretty_print == 'True'):
+
+    if (pretty_print === "True"):
         return "<pre>" + json.dumps(response, indent=2, sort_keys=True) + "</pre>"
     else:
         return jsonify(response)
 
 def api(method, param):
     url = "http://localhost:5279/lbryapi"
-    headers = {'content-type': 'application/json'}
+    headers = { "content-type": "application/json" }
 
     payload = {
         "method": method,
         "params": param
     }
 
-    return requests.post( url, data=json.dumps(payload), headers=headers).json()
+    return requests.post(url, data=json.dumps(payload), headers=headers).json()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
